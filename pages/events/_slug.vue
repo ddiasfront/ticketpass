271932ccdaaa68cbb2b66fb49016e3ae586bbe41
Diego Dias <diegodias@diegos-mbp.home>
2022-06-11 23:30:51 +0100

    Route setup

---
+++ pages/events/_slug.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <li>
+        {{ event.data.name }}
+    </li>
+  </div>
+</template>
+<script>
+export default {
+    
+  async asyncData(params) {
+    const event = await fetch(
+      `http://localhost:8000/api/events/${params.params.slug}`
+    ).then((res) => res.json())
+    return { event }
+  }
+}
+</script>   
---
+++ pages/events/index.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <ul v-for="event in events.data" :key="event.id">
+      <NuxtLink :to="`events/${event.id}`">
+        <li>{{ event.name }}</li>
+      </NuxtLink>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+    
+  async asyncData() {
+    const events = await fetch(
+      'http://localhost:8000/api/events'
+    ).then((res) => res.json())
+    return { events }
+  }
+}
+</script>   
